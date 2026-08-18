e2f6f4cc0195614dd86fa6e8e5297fa7ba8b4782
bdm-k <kokusyunn@gmail.com>
2026-08-19 00:08:36 +0900

    refactor(agnocastlib): consolidate service typesupport loading
    Signed-off-by: bdm-k <kokusyunn@gmail.com>

--- src/agnocastlib/src/agnocast_client.cpp
+++ src/agnocastlib/src/agnocast_client.cpp
@@ -80,33 +80,6 @@ bool wait_for_service_nanoseconds(
   return false;
 }
 
-void GenericClient::load_typesupport_impl(const std::string & service_type)
-{
-  static const std::string ts_introspection_identifier = "rosidl_typesupport_introspection_cpp";
-  const std::string request_type = service_type + "_Request";
-  const std::string response_type = service_type + "_Response";
-
-  ts_lib_introspection_ =
-    rclcpp::get_typesupport_library(service_type, ts_introspection_identifier);
-
-#if RCLCPP_VERSION_MAJOR >= 28
-  const rosidl_message_type_support_t * request_ts = rclcpp::get_message_typesupport_handle(
-    request_type, ts_introspection_identifier, *ts_lib_introspection_);
-  const rosidl_message_type_support_t * response_ts = rclcpp::get_message_typesupport_handle(
-    response_type, ts_introspection_identifier, *ts_lib_introspection_);
-#else
-  const rosidl_message_type_support_t * request_ts = rclcpp::get_typesupport_handle(
-    request_type, ts_introspection_identifier, *ts_lib_introspection_);
-  const rosidl_message_type_support_t * response_ts = rclcpp::get_typesupport_handle(
-    response_type, ts_introspection_identifier, *ts_lib_introspection_);
-#endif
-
-  request_members_ =
-    static_cast<const rosidl_typesupport_introspection_cpp::MessageMembers *>(request_ts->data);
-  response_members_ =
-    static_cast<const rosidl_typesupport_introspection_cpp::MessageMembers *>(response_ts->data);
-}
-
 GenericClient::GenericClient(
   rclcpp::Node * node, const std::string & service_name, const std::string & service_type,
   const rclcpp::QoS & qos, const rclcpp::CallbackGroup::SharedPtr & group, ClientRole role)
@@ -124,7 +97,8 @@ GenericClient::GenericClient(
 ipc_shared_ptr<void> GenericClient::borrow_loaned_request()
 {
   auto generic_request_wrapper = GenericRequestWrapper::allocate(
-    request_members_, [this](size_t size) { return publisher_->borrow_loaned_message(size); });
+    service_ts_bundle_.request_members,
+    [this](size_t size) { return publisher_->borrow_loaned_message(size); });
 
   generic_request_wrapper.node_name() = node_name_;
   generic_request_wrapper.seqno() = next_sequence_number_.fetch_add(1);
@@ -136,7 +110,8 @@ GenericClient::SharedFutureAndRequestId GenericClient::async_send_request(
   ipc_shared_ptr<void> && request, std::function<void(SharedFuture)> && callback)
 {
   SharedFuture shared_future;
-  auto generic_request_wrapper = GenericRequestWrapper(request_members_, std::move(request));
+  auto generic_request_wrapper =
+    GenericRequestWrapper(service_ts_bundle_.request_members, std::move(request));
   int64_t seqno = generic_request_wrapper.seqno();
 
   {
@@ -146,7 +121,7 @@ GenericClient::SharedFutureAndRequestId GenericClient::async_send_request(
   }
 
   publisher_->publish(std::move(generic_request_wrapper).take_request(), [this](void * p) {
-    GenericRequestWrapper::free(p, this->request_members_);
+    GenericRequestWrapper::free(p, this->service_ts_bundle_.request_members);
   });
   return {std::move(shared_future), seqno};
 }
@@ -154,7 +129,8 @@ GenericClient::SharedFutureAndRequestId GenericClient::async_send_request(
 GenericClient::FutureAndRequestId GenericClient::async_send_request(ipc_shared_ptr<void> && request)
 {
   Future future;
-  auto generic_request_wrapper = GenericRequestWrapper(request_members_, std::move(request));
+  auto generic_request_wrapper =
+    GenericRequestWrapper(service_ts_bundle_.request_members, std::move(request));
   int64_t seqno = generic_request_wrapper.seqno();
 
   {
@@ -164,7 +140,7 @@ GenericClient::FutureAndRequestId GenericClient::async_send_request(ipc_shared_p
   }
 
   publisher_->publish(std::move(generic_request_wrapper).take_request(), [this](void * p) {
-    GenericRequestWrapper::free(p, this->request_members_);
+    GenericRequestWrapper::free(p, this->service_ts_bundle_.request_members);
   });
   return {std::move(future), seqno};
 }
@@ -172,7 +148,7 @@ GenericClient::FutureAndRequestId GenericClient::async_send_request(ipc_shared_p
 void GenericClient::cancel_request(ipc_shared_ptr<void> && request)
 {
   publisher_->cancel_message(std::move(request), [this](void * p) {
-    GenericRequestWrapper::free(p, this->request_members_);
+    GenericRequestWrapper::free(p, this->service_ts_bundle_.request_members);
   });
 }
 
